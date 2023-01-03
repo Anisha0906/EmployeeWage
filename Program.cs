b6793d591d2b0951534ba0478c4c95bd8d8ace62
Anisha0906 <anishadas880@gmail.com>
2023-01-03 21:48:11 +0530

    [Anisha]Add:Calculate Total Wage for a month using Switch case

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace EmployeeWageProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage");
-            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage\t 3.Part Time Wage");
+            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage\t 3.Part Time Wage\t 4.Emp Wage for month");
              int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -23,6 +23,9 @@ namespace EmployeeWageProgram
                     break;
                 case 3:
                     PartTimeWage.CalculatePartTimeWage();
+                    break;
+                case 4:
+                    EmpWageSwitchCase.WageForMonth();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong Choice");
